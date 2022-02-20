963b0c40dbfc6a15b66b4ad0f86acf9f1bee285e
Poojani93 <upendrapoojani@gmail.com>
2022-02-20 15:39:43 +0530

    power3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import Header from './Components/Header';
 import './App.css';
+import Navibar from './Components/Navibar';
+import PrintingMap from './Components/PrintingMap';
+import Assembly from './Components/FactoryMaps/Assembly';
+import MainHeader from './Components/MainHeader';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { useState } from 'react'
+import ElectricityCEBM from './Components/ElectricityCEBM';
 
-function App() {
+function App() {  
+
+  
+//   const [machineUrl, setMachineUrl] = useState([
+//     {
+//         mUrl: "http://localhost:5000/getenergy_daily/1",
+//         mUr2: "http://localhost:5000/getenergy_daily/2",
+//         mUr3: "http://localhost:5000/getenergy_daily/3"
+//     }   
+// ])
+const [handleClick, setHandleClick] = useState()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Header /> */}
+        <Navibar />
+        <Routes>
+        <Route path="/printing" element={<PrintingMap setHandleClick={setHandleClick}/>} />
+        <Route path="/assembly" element={<Assembly />} />
+        {/* printing map eke onClick eken ena state eka id kiyala variable ekakata dala prop ekak vidihata header ekata pass karanwa*/}
+        <Route path="/header" element={<MainHeader id={handleClick}/>} />
+        <Route path="/" element={<Header />} />
+        </Routes>
+        {/* <Header /> */}
+      </div>
+      </Router>
   );
+  
 }
 
 export default App;
